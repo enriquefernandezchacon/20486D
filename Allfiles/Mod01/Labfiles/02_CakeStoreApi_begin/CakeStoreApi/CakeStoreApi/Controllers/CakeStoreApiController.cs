3c4e2a4e20d4186593ef5595cbdac1d7a6cc710a
PiAlexay <stanlix@live.ru>
2022-06-06 20:45:17 +0300

    Day_01_Lab_01

---
+++ Allfiles/Mod01/Labfiles/02_CakeStoreApi_begin/CakeStoreApi/CakeStoreApi/Controllers/CakeStoreApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CakeStoreApi.Models;
+using System.Collections.Generic;
+
+namespace CakeStoreApi.Controllers
+{
+    public class CakeStoreApiController : Controller
+    {
+        private IData _data;
+
+        public CakeStoreApiController(IData data)
+        {
+            _data = data;
+        }
+
+        [HttpGet("/api/CakeStore")]
+        public ActionResult<List<CakeStore>> GetAll()
+        {
+            return _data.CakesInitializeData();
+        }
+
+        [HttpGet("/api/CakeStore/{id}", Name = "GetCake")]
+        public ActionResult<CakeStore> GetById(int? id)
+        {
+            var item = _data.GetCakeById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+        
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+    }
+}
