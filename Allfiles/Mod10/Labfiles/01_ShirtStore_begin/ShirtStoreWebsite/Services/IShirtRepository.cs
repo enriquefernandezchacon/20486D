92e4403b8044938568b8ffedc4d86213549ed03a
PiAlexay <stanlix@live.ru>
2022-06-13 18:16:14 +0300

    Day_04_Lab_10

---
+++ Allfiles/Mod10/Labfiles/01_ShirtStore_begin/ShirtStoreWebsite/Services/IShirtRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShirtStoreWebsite.Models;
+using System.Collections.Generic;
+
+namespace ShirtStoreWebsite.Services
+{
+    public interface IShirtRepository
+    {
+
+        IEnumerable<Shirt> GetShirts();
+
+        bool AddShirt(Shirt shirt);
+
+        bool RemoveShirt(int id);
+    }
+}
